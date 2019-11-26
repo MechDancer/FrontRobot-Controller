1c6f2337b72b6216a6ebb9c4a6cff1300477e825
berberman <1793913507@qq.com>
2019-11-26 19:50:52 +0800

    magic, absolutely

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StoneFinder.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StoneFinder.kt
@@ -6,9 +6,7 @@ import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.RADIANS
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit.mmPerInch
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener
@@ -20,7 +18,7 @@ import org.mechdancer.ftclib.util.AutoCallable
 import org.mechdancer.ftclib.util.OpModeLifecycle
 
 
-class StoneFinder :
+class StoneFinder(private val useWebcam: Boolean = true) :
     MonomericStructure("stoneFinder"),
     AutoCallable,
     OpModeLifecycle.Initialize<Robot>,
@@ -32,41 +30,46 @@ class StoneFinder :
     private lateinit var trackables: VuforiaTrackables
 
     companion object {
-        private const val CAMERA_FORWARD_DISPLACEMENT = 4.0f * mmPerInch    // eg: Camera is 4 Inches in front of robot-center
-        private const val CAMERA_VERTICAL_DISPLACEMENT = 8.0f * mmPerInch   // eg: Camera is 8 Inches above ground
-        private const val CAMERA_LEFT_DISPLACEMENT = 0f                             // eg: Camera is ON the robot's center line
-
-        private const val PHONE_X_ROTATE = 0f
-        private const val PHONE_Y_ROTATE = 0f
-        private const val PHONE_Z_ROTATE = 0f
 
         private val VUFORIA_KEY =
-            "AWcj0SH/////AAAAGc51QoZ0Lkf9qsnetlRxwNkVS4T/x5I75ZeQ5CmkxUGRVjxzxR/thQq0i7jaeZX1ZF9cnEsO+NSRhOye+9IQSqYq6TGgc8+iF75ERZAZBKABUd/a6H3ZwBIudWhNdrypQ4r41XjVdb8iBZaTDOqkjw+Zed1ZUkU3dv4HfN/c1nS3/AJLYrgd4zlopvUExBr6ynECCyjgmPXQCcTH6SZ6pph2ZiJXtIBTF/ZaalMXqd9gPMgj9dAy3Hb+Tz26AQFDQevRStLdWZHLdDwdM/a6da7/upBkcMA1dGgm9lQU1UOhneym92qVc3o/eBCKmJ20XehLT2rzbHNNrpHcAqWBSd1d5XW2b8PJIkNRTazIsGCs"
+            "ARUMLrz/////AAABmQJWUPMdxk0js1idkM5EVUAL7NbQ4tLNle+XIBvb776/3jHQz3ZPTqrLUvBD784620pjFp340UGGiVGQrPC+UOSFy3Bd2YXpiLXyD5/N0dRkAi99xPXGtNW9qiAA7gK5fWaDc4xHNEwPVFNLP21g3EQhX5ynDjMyE+togwh+IAT9W0Brc8G0t8eYeFKTMjyG0YhtXZVgtixcjuyC1/is0j4uW6MXzqM17nz2YkBD/mJSnGadaFXNTiMLoPv0EpeKt+dMzjN7+x7+ILeblBg1wVFQz5t1S+KLcEm7JUqemMKg5hx56xXxcdFnnWUteRaZ676UpsN048LZD49EHUb2AV0rE0w6PVpD+F9nJV7y57vA"
 
     }
 
     private val robotFromCamera: OpenGLMatrix = OpenGLMatrix
-        .translation(CAMERA_FORWARD_DISPLACEMENT.toFloat(), CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT.toFloat())
-        .multiplied(Orientation.getRotationMatrix(EXTRINSIC, YZX, DEGREES, PHONE_Y_ROTATE, PHONE_Z_ROTATE, PHONE_X_ROTATE))
+        .translation(0f, 0f, 0f)
+        .let {
+            //            if (useWebcam)
+            it.multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 0f, 0f, 0f))
+//            else
+//                it.multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90f, 0f, -90f))
+        }
 
-
-    var location: ArrayList<Double>? = null
+    var location: DoubleArray? = null
         private set
 
     override fun init(opMode: OpModeWithRobot<Robot>) {
         with(opMode) {
-            val webcamName = hardwareMap["Webcam 1"] as WebcamName
             val cameraMonitorViewId = hardwareMap.appContext.resources.getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.packageName)
             val parameters = VuforiaLocalizer.Parameters(cameraMonitorViewId)
             parameters.vuforiaLicenseKey = VUFORIA_KEY
-            parameters.cameraName = webcamName
+            if (useWebcam) {
+                val webcamName = hardwareMap["Webcam 1"] as WebcamName
+                parameters.cameraName = webcamName
+            } else
+                parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK
             val vuforia = ClassFactory.getInstance().createVuforia(parameters)
             trackables = vuforia.loadTrackablesFromAsset("Skystone")
             val stoneTarget = trackables[0]
             stoneTarget.name = "Stone Target"
             stoneTarget.location = OpenGLMatrix
                 .translation(0f, 0f, 0f)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90f, 0f, 0f))
+                .let {
+                    //                    if (useWebcam)
+                    it.multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 0f, 0f, 0f))
+//                    else
+//                        it.multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90f, 0f, 0f))
+                }
             stoneTargetListener = (stoneTarget.listener as VuforiaTrackableDefaultListener).also { it.setPhoneInformation(robotFromCamera, parameters.cameraDirection) }
             trackables.activate()
         }
@@ -75,17 +78,22 @@ class StoneFinder :
     override fun run() {
         location =
             stoneTargetListener.robotLocation?.inverted()?.run {
-                arrayListOf(
+                val o = Orientation.getOrientation(this, EXTRINSIC, XYZ, RADIANS)
+                doubleArrayOf(
                     translation.get(0).toDouble(),
                     translation.get(1).toDouble(),
-                    Orientation.getOrientation(this, EXTRINSIC, XYZ, RADIANS).thirdAngle.toDouble()
+                    translation.get(2).toDouble(),
+                    o.firstAngle.toDouble(),
+                    o.secondAngle.toDouble(),
+                    o.thirdAngle.toDouble()
                 )
             }
     }
 
 
     override fun stop() {
-        trackables.deactivate()
+        if (this::trackables.isInitialized)
+            trackables.deactivate()
     }
 
     override fun toString(): String = javaClass.simpleName
